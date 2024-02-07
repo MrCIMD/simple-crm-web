e1db64b9bd2df2e48aac06bdccdc76d308636524
Caleb Isaac Mora Diaz <calexico659@gmail.com>
2024-02-06 22:41:27 -0500

    feat - se configuran los iconos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import {Component} from '@angular/core';
 import {RouterOutlet} from '@angular/router';
+import {MatIconModule, MatIconRegistry} from '@angular/material/icon';
 import {DashboardLayoutComponent} from "./components/dashboard-layout/dashboard-layout.component";
+import {DomSanitizer} from "@angular/platform-browser";
+import {HttpClientModule} from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, DashboardLayoutComponent],
+  imports: [RouterOutlet, MatIconModule, HttpClientModule, DashboardLayoutComponent],
   templateUrl: './app.component.html',
   styles: ''
 })
 export class AppComponent {
+  constructor(
+    private readonly iconRegistry: MatIconRegistry,
+    private readonly domSanitizer: DomSanitizer
+  ) {
+    this.iconRegistry.addSvgIconSet(
+      this.domSanitizer.bypassSecurityTrustResourceUrl('./assets/mdi.svg')
+    )
+  }
 }
--- src/app/components/dashboard-layout/dashboard-layout.component.ts
+++ src/app/components/dashboard-layout/dashboard-layout.component.ts
@@ -2,12 +2,12 @@ import {Component} from '@angular/core';
 import {MatSidenavModule} from "@angular/material/sidenav";
 import {MatButtonModule} from "@angular/material/button";
 import {MatToolbarModule} from "@angular/material/toolbar";
-
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-dashboard-layout',
   standalone: true,
-  imports: [MatSidenavModule, MatButtonModule, MatToolbarModule],
+  imports: [MatSidenavModule, MatButtonModule, MatToolbarModule, MatIcon],
   templateUrl: './dashboard-layout.component.html',
   styles: ``
 })
