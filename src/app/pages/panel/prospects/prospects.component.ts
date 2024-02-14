2065a2b2670742bb116cb555093be3a3e4197cff
Caleb Isaac Mora Diaz <calexico659@gmail.com>
2024-02-14 15:39:24 -0500

    feat - se agregar el drag and drop de columnas del crm

--- src/app/pages/panel/prospects/prospects.component.ts
+++ src/app/pages/panel/prospects/prospects.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
-import { DragDropModule } from '@angular/cdk/drag-drop';
+import { CdkDragDrop, DragDropModule, moveItemInArray } from '@angular/cdk/drag-drop';
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
   selector: 'app-prospects',
   standalone: true,
-  imports: [DragDropModule],
+  imports: [DragDropModule, MatButtonModule, MatIconModule],
   templateUrl: './prospects.component.html',
-  styles: ``
+  styleUrl: './prospects.component.scss'
 })
 export default class ProspectsComponent {
+  timePeriods = [
+    'Bronze age',
+    'Iron age',
+    'Middle ages',
+    'Large ages',
+  ];
 
+  public drop(event: CdkDragDrop<string[]>): void {
+    moveItemInArray(this.timePeriods, event.previousIndex, event.currentIndex);
+  }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,25 @@ module.exports = {
     "./src/**/*.{html,scss,ts}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          '50': '#f3e5f5',
+          '100': '#e1bee7',
+          '200': '#ce93d8',
+          '300': '#ba68c8',
+          '400': '#ab47bc',
+          '500': '#9c27b0',
+          '600': '#8e24aa',
+          '700': '#7b1fa2',
+          '800': '#6a1b9a',
+          '900': '#4a148c',
+        }
+      },
+      transitionTimingFunction: {
+        "cubic-bezier": "cubic-bezier(0, 0, 0.2, 1)"
+      }
+    },
   },
   plugins: [],
 }
